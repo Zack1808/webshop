f87bd6c15545477d18534dd491bbc4bb60df64b4
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-10-26 06:39:51 +0200

    Recreated the src folder

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Importing the style file
+import '../assets/css/App.css';
+
+// Creating the App component
+const App = () => {
+    return (
+        <div className="container">App</div>
+    )
+}
+
+// exporting the App component
+export default App;
