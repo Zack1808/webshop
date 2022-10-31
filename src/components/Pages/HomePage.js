3905bab17b082b2814044cbfe5ca185f3454ebca
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-10-31 08:33:56 +0100

    Added subcategory listing

--- src/components/Pages/HomePage.js
+++ src/components/Pages/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // Importing the style file
 import '../../assets/css/HomePage.css'
@@ -13,20 +13,53 @@ import CategorySelection from './CategorySelection';
 const HomePage = ({ categories, products}) => {
 
     // Initializing state 
-    const [selectedCategory, setSelectedCategory] = useState("...Select Category");
+    const [selectedCategory, setSelectedCategory] = useState("");
+    const [subCategories, setSubCategories] = useState([]);
+
+    // Fetching the subcategories once every time a new category is selected
+    useEffect(() => {
+        setSubCategories(fetchSubCategories(selectedCategory))
+    }, [selectedCategory])
+
+    // Function that will fetch the subcategories
+    const fetchSubCategories = (id) => {
+        if(selectedCategory) {
+            let index;
+            for(var i = 0; i < categories.length; i++) {
+                if(categories[i].id === id) index = i;
+            }
+            return categories[index].children;
+        }
+        return [];
+    }
+
+    // Function that will display the selected category
+    const displayCategory = (id) => {
+        if(selectedCategory) {
+            let index;
+            for(var i = 0; i < categories.length; i++) {
+                if(categories[i].id === id) index = i;
+            }
+            return categories[index].name;
+        }
+        return "...Select Category";
+    } 
+
+    // Varialbe that will hold the name of the selected category
+    let categoryName = displayCategory(selectedCategory)
 
     return (
         <div className="home-container">
             <header className="home-header">
-                <Select categories={categories} selected={selectedCategory} setSelected={setSelectedCategory} />
+                <Select items={categories} selected={categoryName} setSelected={setSelectedCategory} />
                 <Search />
             </header>
             <div className="lists-container">
                 {
-                    selectedCategory === "...Select Category" ? (
+                    !selectedCategory ? (
                         <CategorySelection categories={categories} setSelected={setSelectedCategory} />
                     ) : (
-                        <div />
+                        <Filter categories={subCategories} />
                     )
                 }
             </div>
