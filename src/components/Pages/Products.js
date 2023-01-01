313f21d1c54c1fc41a4afab7acd4bb5f33f43d89
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-01-01 06:26:37 +0100

    Added the subcategory selection again

--- src/components/Pages/Products.js
+++ src/components/Pages/Products.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 // Importing the css file
 import '../../assets/css/Products.css'
@@ -12,23 +12,48 @@ import Search from '../Search';
 
 // Importing helper functions
 import { renderCategory } from '../../assets/data/renderFunctions';
+import { fetchSubCategories } from '../../assets/data/fetchingFunctions';
 
 // Creating the Products component
 const Products = ({ properties}) =>{
 
     // Variable and state definition start
     var categoryName = renderCategory(properties.selectedCategory, properties.categories)
+    const [subCategories, setSubCategories] = useState([])
+    const [selectedSubCategories, setSelectedSubCategories] = useState([])
     // Variable and state definition end
 
     // useEffect functionst start
     useEffect(() => {
-        if(properties.selectedCategory !== "") localStorage.setItem("react-webshop-selected-category", JSON.stringify(properties.selectedCategory))
+        if(properties.selectedCategory !== "") {
+            localStorage.setItem("react-webshop-selected-category", JSON.stringify(properties.selectedCategory))
+            setSubCategories(fetchSubCategories(properties.selectedCategory, properties.categories))
+        }
         properties.setSelectedCategory(JSON.parse(localStorage.getItem("react-webshop-selected-category")))
+
     }, [])
 
     useEffect(() => {
         localStorage.setItem("react-webshop-selected-category", JSON.stringify(properties.selectedCategory))
+        setSubCategories(fetchSubCategories(properties.selectedCategory, properties.categories))
     }, [properties.selectedCategory])
+    // useEffect functions end
+
+    // Functions start
+    // Function that adds a subcategory to the list of selected subcategories
+    const addSubCategory = (slug) => {
+        var added = [...selectedSubCategories]
+        added.push(slug)
+        setSelectedSubCategories(added)
+    }
+
+    // Functions that removes a subcategory from the list of selected subcategories
+    const removeSubCategory = (slug) => {
+        var removed = [...selectedSubCategories]
+        removed.splice(removed.indexOf(slug), 1)
+        setSelectedSubCategories(removed)
+    }
+    // Functions 
 
     return (
         <div className="products-container">
@@ -36,14 +61,14 @@ const Products = ({ properties}) =>{
                 <Select items={properties.categories} selected={categoryName} setSelected={properties.setSelectedCategory} />
                 <Search />
             </header>
-            {/* <div className='home-products'>
-                <Filter categories={subCategories} sorting={sorting} remove={removeSubCategory} add={addSubCategory} products={products} />
-                {selectedProducts.length !== 0 ? (
+            <div className='home-products'>
+                <Filter categories={subCategories} add={addSubCategory} remove={removeSubCategory} />
+                {/* {selectedProducts.length !== 0 ? (
                     <ProductList products={selectedProducts} />
                 ) : (
                     <ProductList products={products} />
-                )}
-            </div> */}
+                )} */}
+            </div>
         </div>
     ) 
 }
