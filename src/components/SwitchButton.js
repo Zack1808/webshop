ee71c6c05838a8e5df7c9f05f4722fc7712f4a48
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-01-01 06:03:36 +0100

    Fixed the slider button

--- src/components/SwitchButton.js
+++ src/components/SwitchButton.js
@@ -8,8 +8,8 @@ import '../assets/css/SwitchButton.css'
 const spring = {
     type: "spring",
     stiffness: 600,
-    damping: 150,
-    duration: .4,
+    damping: 170,
+    duration: .6,
 }
 
 // Creating the wariants for darkmode toggle
@@ -17,12 +17,16 @@ const dark = {
     backgroundColor: "rgb(60, 60, 52)",
     boxShadow: "inset -3px -4px 0 1px #f3d076",
     rotate: [0, 360],
+    x: 20,
+    y: 0,
 }
 
 // Creating the wariants for lightmode toggle
 const light = {
     backgroundColor: "rgb(250, 244, 187)",
     rotate: [360, 0],
+    x: 0,
+    y: 0,
 }
 
 // Creating the SwitchButton component
