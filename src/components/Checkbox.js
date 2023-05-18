59d8ddb0dafb3cee5ffd085deabcdad8781d4e59
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-05-18 18:29:31 +0200

    Removed issues for netlify deploy

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,37 +1,45 @@
-import React, { useState, useEffect } from 'react'
-import { UilCheck } from '@iconscout/react-unicons'
+import React, { useState, useEffect } from "react";
+import { UilCheck } from "@iconscout/react-unicons";
 
 // Importing fetching functions
-import { fetchCategoryProductCount } from '../assets/data/fetchingFunctions';
+import { fetchCategoryProductCount } from "../assets/data/fetchingFunctions";
 
 // importing the style file
-import '../assets/css/CheckBox.css'
+import "../assets/css/CheckBox.css";
 
 // Creating the CheckBox component
 const CheckBox = ({ item, remove, add }) => {
+  useEffect(() => {
+    fetchCategoryProductCount(item.slug, setAmount);
 
-    useEffect(() => {
-        fetchCategoryProductCount(item.slug, setAmount)
-    }, [])
+    //eslint-disable-next-line
+  }, []);
 
-    // Defining states
-    const [toggle, setToggle] = useState(false);
-    const [amount, setAmount] = useState(0);
+  // Defining states
+  const [toggle, setToggle] = useState(false);
+  const [amount, setAmount] = useState(0);
 
-    // Function that will handle the click event 
-    const handleClick = () => {
-        setToggle(previousState => !previousState);
-        if(!toggle) add(item.slug)
-        else remove(item.slug)
-    }
+  // Function that will handle the click event
+  const handleClick = () => {
+    setToggle((previousState) => !previousState);
+    if (!toggle) add(item.slug);
+    else remove(item.slug);
+  };
 
-    return (
-        <div className="checkbox-container">
-            <div className={`checkbox ${toggle && "checkbox-active"}`} onClick={handleClick}><UilCheck className={!toggle ? "not-active" : ""} /></div>
-            <label onClick={handleClick}>{item.name} <small>{item.assets && `(${amount})`}</small></label>
-        </div>
-    )
-}
+  return (
+    <div className="checkbox-container">
+      <div
+        className={`checkbox ${toggle && "checkbox-active"}`}
+        onClick={handleClick}
+      >
+        <UilCheck className={!toggle ? "not-active" : ""} />
+      </div>
+      <label onClick={handleClick}>
+        {item.name} <small>{item.assets && `(${amount})`}</small>
+      </label>
+    </div>
+  );
+};
 
 // Exporting the CheckBox component
 export default CheckBox;
--- src/components/Pages/HomePage.js
+++ src/components/Pages/HomePage.js
@@ -1,36 +1,40 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 
 // Importing the style file
-import '../../assets/css/HomePage.css'
+import "../../assets/css/HomePage.css";
 
 // Importing the costume made components
-import CategorySelection from './CategorySelection';
-import Loader from '../Loader';
+import CategorySelection from "./CategorySelection";
+import Loader from "../Loader";
 
-// Creating the HomePage component 
+// Creating the HomePage component
 const HomePage = ({ properties }) => {
+  // useEffect functions start
+  useEffect(() => {
+    properties.setSelectedCategory("");
 
-    // useEffect functions start
-    useEffect(() => {
-        properties.setSelectedCategory("")
-    }, [])
-    
+    // eslint-disable-next-line
+  }, []);
 
-    return (
-        <div className="home-container">
+  return (
+    <div className="home-container">
+      <h1>Select a category</h1>
 
-            <h1>Select a category</h1>
-
-            {/* Category listing start */}
-            <div className="lists-container">
-                {
-                    properties.categories.length !== 0 ? <CategorySelection categories={properties.categories} setSelected={properties.setSelectedCategory} /> : <Loader />
-                }
-            </div>
-            {/* Category listing end */}
-        </div>
-    )
-}
+      {/* Category listing start */}
+      <div className="lists-container">
+        {properties.categories.length !== 0 ? (
+          <CategorySelection
+            categories={properties.categories}
+            setSelected={properties.setSelectedCategory}
+          />
+        ) : (
+          <Loader />
+        )}
+      </div>
+      {/* Category listing end */}
+    </div>
+  );
+};
 
 // Exporting the HomePage component
 export default HomePage;
