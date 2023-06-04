926bb10c3faaf0683184fb86638ab139d2f773c3
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-06-04 18:22:27 +0200

    Added the intersection observer on the CategoryItem

--- src/components/Home/CategoryList/CategoryItem/CategoryItem.js
+++ src/components/Home/CategoryList/CategoryItem/CategoryItem.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { useInView } from "react-intersection-observer";
 
 // Importing the style file
 import "./CategoryItem.css";
 
 // Creating the CategoryItem component
 const CategoryItem = ({ category }) => {
+  // Setting up the intersection observer
+  const [ref, inView] = useInView({ threshold: 0.1 });
+
   return (
-    <div className="category-item-container">
+    <div
+      ref={ref}
+      className={`category-item-container ${inView ? "visible" : ""}`}
+    >
       <img src={category.assets[0].url} alt="" />
       <h2>{category.name}</h2>
       <button>Check it out</button>
