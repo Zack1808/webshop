3c6a8078dd33e7dccd09acf2b525de5eec243da1
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-07-07 21:38:19 +0200

    added responsivenes

--- src/components/ProductsList/Sidebar/Sidebar.js
+++ src/components/ProductsList/Sidebar/Sidebar.js
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import { useLocation } from "react-router-dom";
+import { UilAngleDown } from "@iconscout/react-unicons";
 
 // Importing the costume components
 import Checkbox from "./Checkbox/Checkbox";
@@ -18,6 +19,9 @@ const Sidebar = () => {
   // Setting up state
   const [id, setId] = useState("");
   const [active, setActive] = useState(true);
+  const [radio, setRadio] = useState(false);
+  const [check, setCheck] = useState(false);
+  const [form, setForm] = useState(false);
 
   // Creating the intersection observer reference
   const [ref, inView] = useInView();
@@ -72,36 +76,58 @@ const Sidebar = () => {
       }}
     >
       <div ref={ref} className={`sidebar-container ${inView ? "visible" : ""}`}>
-        <form>
-          <h3>Categories</h3>
-          {id &&
-            categories.map((category) => {
-              if (category.id === id)
-                return category.children.map((child) => (
-                  <Checkbox
-                    key={child.id}
-                    label={child.name}
-                    slug={child.slug}
-                  />
-                ));
-              else return null;
-            })}
-        </form>
-        <form>
-          <h3>Sort by price</h3>
-          <RadioButton
-            name="asc"
-            label="Sort from lowest to highest"
-            active={active}
-            updateChecked={updateChecked}
-          />
-          <RadioButton
-            name="desc"
-            label="Sort from highest to lowest"
-            active={!active}
-            updateChecked={updateChecked}
-          />
-        </form>
+        <h2 onClick={() => setForm((prevState) => !prevState)}>
+          Filters{" "}
+          <span className={form ? "active" : ""}>
+            <UilAngleDown />
+          </span>
+        </h2>
+        <div className={`forms ${form ? "active" : ""}`}>
+          <form>
+            <h3 onClick={() => setCheck((prevState) => !prevState)}>
+              Categories{" "}
+              <span className={check ? "active" : ""}>
+                <UilAngleDown />
+              </span>
+            </h3>
+            <div className={`checks ${check ? "active" : ""}`}>
+              {id &&
+                categories.map((category) => {
+                  if (category.id === id)
+                    return category.children.map((child) => (
+                      <Checkbox
+                        key={child.id}
+                        label={child.name}
+                        slug={child.slug}
+                      />
+                    ));
+                  else return null;
+                })}
+            </div>
+          </form>
+          <form>
+            <h3 onClick={() => setRadio((prevState) => !prevState)}>
+              Sort by price
+              <span className={radio ? "active" : ""}>
+                <UilAngleDown />
+              </span>
+            </h3>
+            <div className={`radios ${radio ? "active" : ""}`}>
+              <RadioButton
+                name="asc"
+                label="Sort from lowest to highest"
+                active={active}
+                updateChecked={updateChecked}
+              />
+              <RadioButton
+                name="desc"
+                label="Sort from highest to lowest"
+                active={!active}
+                updateChecked={updateChecked}
+              />
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
